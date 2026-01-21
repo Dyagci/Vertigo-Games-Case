4f7025c31af92d40c7f702aea95263a6eb874e39
Denizhan <denizhan1999@hotmail.com>
2026-01-21 04:55:52 +0300

    Created Slice Placer
    Automatically places slices image and text to wheels slice locations

---
+++ Assets/Scripts/UI/WheelSlicePositioner.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WheelSlicePositioner : MonoBehaviour
+{
+    [SerializeField] private float radius = 150f;
+    [SerializeField] private float textOffset = 30f;
+
+    private void OnValidate()
+    {
+        PositionSlices();
+    }
+
+    private void PositionSlices()
+    {
+        int sliceCount = transform.childCount;
+        
+        for (int i = 0; i < sliceCount; i++)
+        {
+            RectTransform slice = transform.GetChild(i) as RectTransform;
+            if (slice == null) continue;
+            
+            float angle = i * (360f / sliceCount);
+            float rad = angle * Mathf.Deg2Rad;
+            
+            float x = Mathf.Sin(rad) * radius;
+            float y = Mathf.Cos(rad) * radius;
+            
+            slice.anchoredPosition = new Vector2(x, y);
+            slice.localRotation = Quaternion.Euler(0, 0, -angle);
+            
+            // Text is second child (index 1), push toward center
+            if (slice.childCount > 1)
+            {
+                RectTransform textRect = slice.GetChild(1) as RectTransform;
+                if (textRect != null)
+                {
+                    textRect.anchoredPosition = new Vector2(0, -textOffset);
+                }
+            }
+        }
+    }
+}
